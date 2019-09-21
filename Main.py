f5a7b6a935f44097f9b0e89b0d6f8168f3ff096d
Hadis <jalesiyan.hadis@yahoo.com>
2019-09-21 18:44:14 +0430

    Add files via upload
    first run.

---
+++ Main.py
@@ -0,0 +1,180 @@
+#%load_ext autoreload
+#%aimport helper, tests
+#%autoreload 1
+#instead of the previouse command
+
+from IPython import get_ipython
+ipython = get_ipython()
+
+if '__IPYTHON__' in globals():
+    ipython.magic('load_ext autoreload')
+    ipython.magic('autoreload 1')
+
+
+import helper
+import numpy as np
+import project_tests as tests
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import GRU, Dense, TimeDistributed, RepeatVector, Bidirectional, Dropout
+from keras.layers.embeddings import Embedding
+from keras.optimizers import Adam
+from keras.losses import sparse_categorical_crossentropy
+
+#Verify access to the GPU
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
+# Load English data
+english_sentences = helper.load_data('data/small_vocab_en')
+# Load French data
+french_sentences = helper.load_data('data/small_vocab_fr')
+print('Dataset Loaded')
+
+def tokenize(x):
+    """
+    Tokenize x
+    :param x: List of sentences/strings to be tokenized
+    :return: Tuple of (tokenized x data, tokenizer used to tokenize x)
+    """
+    # TODO: Implement
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(x)
+    return tokenizer.texts_to_sequences(x), tokenizer
+#What is doing this code?
+#tests.test_tokenize(tokenize)
+
+def pad(x, length=None):
+    """
+    Pad x
+    :param x: List of sequences.
+    :param length: Length to pad the sequence to.  If None, use length of longest sequence in x.
+    :return: Padded numpy array of sequences
+    """
+    # TODO: Implement
+    return pad_sequences(x, maxlen=length, padding='post')
+
+#tests.test_pad(pad)
+
+#PreProcess PipeLine
+    
+def preprocess(x, y):
+    """
+    Preprocess x and y
+    :param x: Feature List of sentences
+    :param y: Label List of sentences
+    :return: Tuple of (Preprocessed x, Preprocessed y, x tokenizer, y tokenizer)
+    """
+    preprocess_x, x_tk = tokenize(x)
+    preprocess_y, y_tk = tokenize(y)
+
+    preprocess_x = pad(preprocess_x)
+    preprocess_y = pad(preprocess_y)
+
+    # Keras's sparse_categorical_crossentropy function requires the labels to be in 3 dimensions
+    preprocess_y = preprocess_y.reshape(*preprocess_y.shape, 1)
+
+    return preprocess_x, preprocess_y, x_tk, y_tk
+
+preproc_english_sentences, preproc_french_sentences, english_tokenizer, french_tokenizer =\
+    preprocess(english_sentences, french_sentences)
+    
+max_english_sequence_length = preproc_english_sentences.shape[1]
+max_french_sequence_length = preproc_french_sentences.shape[1]
+english_vocab_size = len(english_tokenizer.word_index)+1
+french_vocab_size = len(french_tokenizer.word_index)+1
+
+
+
+#Ids Back to Text
+def logits_to_text(logits, tokenizer):
+    """
+    Turn logits from a neural network into text using the tokenizer
+    :param logits: Logits from a neural network
+    :param tokenizer: Keras Tokenizer fit on the labels
+    :return: String that represents the text of the logits
+    """
+    index_to_words = {id: word for word, id in tokenizer.word_index.items()}
+    index_to_words[0] = '<PAD>'
+
+    return ' '.join([index_to_words[prediction] for prediction in np.argmax(logits, 1)])
+
+print('`logits_to_text` function loaded.')
+#Simple Model
+
+def model_final(input_shape, output_sequence_length, english_vocab_size, french_vocab_size):
+    """
+    Build and train a model that incorporates embedding, encoder-decoder, and bidirectional RNN on x and y
+    :param input_shape: Tuple of input shape
+    :param output_sequence_length: Length of output sequence
+    :param english_vocab_size: Number of unique English words in the dataset
+    :param french_vocab_size: Number of unique French words in the dataset
+    :return: Keras model built, but not trained
+    """
+    # TODO: Implement
+
+    # Hyperparameters
+    learning_rate = 0.003
+    
+    # Build the layers    
+    model = Sequential()
+    # Embedding
+    model.add(Embedding(english_vocab_size, 128, input_length=input_shape[1],
+                         input_shape=input_shape[1:]))
+    # Encoder
+    model.add(Bidirectional(GRU(128)))
+    model.add(RepeatVector(output_sequence_length))
+    # Decoder
+    model.add(Bidirectional(GRU(128, return_sequences=True)))
+    model.add(TimeDistributed(Dense(512, activation='relu')))
+    model.add(Dropout(0.5))
+    model.add(TimeDistributed(Dense(french_vocab_size, activation='softmax')))
+    model.compile(loss=sparse_categorical_crossentropy,
+                  optimizer=Adam(learning_rate),
+                  metrics=['categorical_accuracy'])
+    return model
+
+tests.test_model_final(model_final)
+
+print('Final Model Loaded')
+
+
+
+def final_predictions(x, y, x_tk, y_tk):
+    """
+    Gets predictions using the final model
+    :param x: Preprocessed English data
+    :param y: Preprocessed French data
+    :param x_tk: English tokenizer
+    :param y_tk: French tokenizer
+    """
+    # TODO: Train neural network using model_final
+    model = model_final(x.shape,y.shape[1],
+                        len(x_tk.word_index)+1,
+                        len(y_tk.word_index)+1)
+    model.summary()
+    model.fit(x, y, batch_size=1024, epochs=25, validation_split=0.2)
+
+    
+    ## DON'T EDIT ANYTHING BELOW THIS LINE
+    y_id_to_word = {value: key for key, value in y_tk.word_index.items()}
+    y_id_to_word[0] = '<PAD>'
+
+    sentence = 'he saw a old yellow truck'
+    sentence = [x_tk.word_index[word] for word in sentence.split()]
+    sentence = pad_sequences([sentence], maxlen=x.shape[-1], padding='post')
+    sentences = np.array([sentence[0], x[0]])
+    predictions = model.predict(sentences, len(sentences))
+
+    print('Sample 1:')
+    print(' '.join([y_id_to_word[np.argmax(x)] for x in predictions[0]]))
+    print('Il a vu un vieux camion jaune')
+    print('Sample 2:')
+    print(' '.join([y_id_to_word[np.argmax(x)] for x in predictions[1]]))
+    print(' '.join([y_id_to_word[np.max(x)] for x in y[0]]))
+
+
+final_predictions(preproc_english_sentences, preproc_french_sentences, english_tokenizer, french_tokenizer)
+

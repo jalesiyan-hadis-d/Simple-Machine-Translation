ee2fcee3130f2c0644445791a4cdf21a4d28f2b0
Hadis <jalesiyan.hadis@yahoo.com>
2019-10-17 12:33:48 +0330

    improved Bleu score

--- Evaluation.py
+++ Evaluation.py
@@ -11,7 +11,7 @@ if '__IPYTHON__' in globals():
 
 import helper
 import numpy as np
-import nltk
+#import nltk
 
 #Verify access to the GPU
 from tensorflow.python.client import device_lib
@@ -26,15 +26,16 @@ X_train, X_test, Y_train, Y_test=helper.Pickle_out_data("Split_Test_Train_Data.p
 
 #model=helper.load_model("models/Glove.json")
 
-model=helper.load_model("models/Glove-LSTM.json")
+model=helper.load_model("models/Glove.json")
 model.summary()
 
 #Predicting Test data
 
 Predicted_sentences = model.predict(X_test, len(X_test))
 
-
-
+## DON'T EDIT ANYTHING BELOW THIS LINE
+y_id_to_word = {value: key for key, value in target_tokenizer.word_index.items()}
+y_id_to_word[0] = '<PAD>'
 #Bleu
 
 predict_list=[]
@@ -43,19 +44,30 @@ for lst in Predicted_sentences:
 
 Y_test=Y_test.tolist()
 
-nltk.translate.bleu_score.corpus_bleu(Y_test, predict_list)
 
 
+test_sentence=[]
+predict_sentce=[]
+size = len(Y_test)
+for i in range (0,size):
+   test_sentence.append(([y_id_to_word[np.max(x)] for x in Y_test[i]]))
+   predict_sentce.append(([y_id_to_word[np.max(x)] for x in predict_list[i]]))
 
-## DON'T EDIT ANYTHING BELOW THIS LINE
-y_id_to_word = {value: key for key, value in target_tokenizer.word_index.items()}
-y_id_to_word[0] = '<PAD>'
-"""
-for i in range (1,10):
-    print('source %f:', i)
-    print(' '.join([y_id_to_word[np.max(x)] for x in Y_test[i]]))
+#nltk.translate.bleu_score.corpus_bleu(Y_test, predict_list)
+
+from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import SmoothingFunction
+#weights = (0.5,0.5,0,0)
+chencherry = SmoothingFunction()
+#weights=(0.5,0.5,0,0)
+#compare elements
+#print ("compare:", hypo)
+score=[]
+#print(('-')*40,'\n')
+for i in range(0,size):
+    ref=test_sentence[i]
+    hypo=predict_sentce[i]
+    score.append(sentence_bleu([ref], hypo, smoothing_function=chencherry.method4))
+    #print (corpus[i], 'has a similarity rate of:\t', score, '%')
     
-    print('Translate %f:',i)
-    print(' '.join([y_id_to_word[np.max(x)] for x in predict_list[i]]))
-
-"""
+np.average(score)
